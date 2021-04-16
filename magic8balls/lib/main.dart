2ca3ec28183f2cc38efdd1e271bac0840119d89b
Abhishek Kumar <flashtoak@gmail.com>
2021-04-16 15:09:17 +0530

    a flutter project

---
+++ magic8balls/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text('8 Magic Balls'),
+        ),
+        body: MagicBallPage(),
+      ),
+    ),
+  );
+}
+
+class MagicBallPage extends StatefulWidget {
+  @override
+  _MagicBallPageState createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int magicNo = 1;
+  void changeStateMagic() {
+    setState(() {
+      magicNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextButton(
+          onPressed: () {
+            changeStateMagic();
+          },
+          child: Image.asset('images/ball$magicNo.png'),
+        ),
+        I
+      ],
+    );
+  }
+}
